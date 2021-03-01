2fc201a0dda42b9c9c354e46a89b47edd83e3981
banu tryon <banutryon@banus-mbp.lan>
2021-03-01 11:49:27 -0800

    setup

---
+++ email form with firebase api/firebase.js
@@ -0,0 +1,15 @@
+// import firebase from "firebase";
+
+// var firebaseApp = firebase.initializeApp({
+// 	apiKey: "AIzaSyDEN_0GZ4m-Vd135teXyCh-hAGPyneyeAg",
+// 	authDomain: "banu-portfolio.firebaseapp.com",
+// 	projectId: "banu-portfolio",
+// 	storageBucket: "banu-portfolio.appspot.com",
+// 	messagingSenderId: "251811722946",
+// 	appId: "1:251811722946:web:3b1707c80fcdd7e654ed5b",
+// 	measurementId: "G-WKHLGTW3HY",
+// });
+
+// var dataBase = firebaseApp.firestore();
+
+// export { dataBase };
